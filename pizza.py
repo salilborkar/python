a75382a4604c4161d3b5012eeee707695d96c08d
salilborkar <salilborkar@users.noreply.github.com>
2020-07-16 10:59:55 -0700

    Update pizza.py

--- pizza.py
+++ pizza.py
@@ -1,23 +1,28 @@
 #pizza py
 
-name = input("what is your name? ")
-print(f"hello, {name} !")
+#name = input("what is your name? ")
+#print(f"hello, {name} !")
 
-crust = input("which crust would you like to order? ")
-print(f"your pizza will be made using {crust} crust pizza! ")
+#crust = input("which crust would you like to order? ")
+#print(f"your pizza will be made using {crust} crust pizza! ")
 
-top = ""
-toppings = []
+#top = ""
+#toppings = []
 
-while top != 'quit':
-	top = input("what are the toppings do you need ")
-	toppings.append(top)
-	for topping in toppings:
-		print(f"added {topping} ")
-	#if message == "quit":
-	#	break
+#while top != 'quit':
+#	top = input("what are the toppings do you need ")
+#	toppings.append(top)
+#	for topping in toppings:
+#		print(f"added {topping} ")
+#	#if message == "quit":
+#	#	break
 
-print(f" Hi, {name} your final order is ")
-print(f"{crust} crust pizza with ")
-print(toppings)
-print("toppings. thank you for the order")
+def make_pizza(*toppings):
+#	print(f" Hi, {name} your final order is ")
+#	print(f"{crust} crust pizza with ")
+	print("making pizza using")
+	print(toppings)
+#	print("toppings. thank you for the order")
+
+make_pizza('onion')
+make_pizza('chicken','pineapple')
